4f0bc90dc1df03c46e49ee4532f6445e39febf31
Florian Jomain <florian.jomain@gmail.com>
2020-04-23 18:06:52 +0200

    All basic interactions

--- routes/auth.js
+++ routes/auth.js
@@ -10,62 +10,59 @@ router.get("/signup", (req, res) => {
     res.render("signup.hbs");
 });
 
-router.post(
-    "/signup",
-    (req, res) => {
-        const {
-            name,
-            lastName,
-            email,
-            password
-        } = req.body;
+router.post("/signup", (req, res) => {
+    const {
+        name,
+        lastName,
+        email,
+        password
+    } = req.body;
 
-        console.log(req.body);
-        const salt = bcrypt.genSaltSync(bcryptSalt);
-        const hashPass = bcrypt.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
 
-        if (name === "" || lastName === "" || email === "" || password === "") {
-            res.render("signup.hbs", {
-                msg: "Please fill all information",
-            });
-        }
-
-        //   User.findOne({
-        //     email: email,
-        //   })
-        //     .then((user) => {
-        //       console.log("I am here");
-
-        //       if (user !== null) {
-        //         res.render("signup.hbs", {
-        //           msg: "This email address already exists.",
-        //         });
-        //       } else {
-        User.create({
-                name,
-                lastName,
-                email,
-                password: hashPass,
-            })
-            .then((dbResult) => {
-                console.log(dbResult);
-                res.redirect("/sneakers/collection");
-            })
-            .catch((dbErr) => {
-                console.log("This is a creation error");
-            });
+    if (name === "" || lastName === "" || email === "" || password === "") {
+        res.render("signup.hbs", {
+            msg: "Please fill all information",
+        });
     }
-    // })
-    // .catch((dbErr) => {
-    //   console.log("This is a matching error");
-    // });
-);
+
+    User.findOne({
+            email: email,
+        })
+        .then(user => {
+            console.log("I am here");
+
+            if (user !== null) {
+                res.render("signup.hbs", {
+                    msg: "This email address already exists.",
+                });
+            } else {
+                User.create({
+                        name,
+                        lastName,
+                        email,
+                        password: hashPass,
+                    })
+                    .then((dbResult) => {
+                        console.log(dbResult);
+                        res.redirect("/sneakers/collection");
+                    })
+                    .catch((dbErr) => {
+                        console.log("This is a creation error");
+                    });
+            }
+        })
+        .catch((dbErr) => {
+            console.log("This is a matching error");
+        });
+});
 
 router.get("/login", (req, res) => {
     res.render("login.hbs");
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", (req, res, next) => {
     const {
         email,
         password
@@ -89,7 +86,7 @@ router.post("/login", (req, res) => {
 
             if (bcrypt.compareSync(password, user.password)) {
                 req.session.currentUser = user;
-                res.redirect("/");
+                res.redirect('/sneakers/collection')
             } else {
                 res.render("login.hbs", {
                     msg: "Invalid credentials",
